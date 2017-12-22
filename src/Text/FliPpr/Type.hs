0930473339d3d951406b9fa394c4b89ab76a7ed2
Kazutaka Matsuda <kztk@ecei.tohoku.ac.jp>
2017-12-23 00:14:36 +0900

    .

--- src/Text/FliPpr/Type.hs
+++ src/Text/FliPpr/Type.hs
@@ -1,15 +1,4 @@
-{-# LANGUAGE UndecidableInstances #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE ExistentialQuantification #-}
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies #-}
-{-# LANGUAGE KindSignatures #-}
-{-# LANGUAGE GADTs #-}
-
-module Text.FliPpr.Type
+module Text.FliPpr.Type (
+  )
 where
 
-import Text.FliPpr.Internal.Type
-import Text.FliPpr.Internal.PrettyPrinting 
-import Text.FliPpr.Internal.ParserGeneration 
