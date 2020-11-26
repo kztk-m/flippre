462a08dff2a6cc806743ce20ce755154986e60da
Kazutaka Matsuda <kztk@ecei.tohoku.ac.jp>
2020-11-26 20:36:55 +0900

    Made PrettyPrinting interpretation compatible with the new design.

--- src/Text/FliPpr/Internal/PrettyPrinting.hs
+++ src/Text/FliPpr/Internal/PrettyPrinting.hs
@@ -1,20 +1,18 @@
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE KindSignatures #-}
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE GADTs #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 
-{-# LANGUAGE MultiParamTypeClasses #-}
 module Text.FliPpr.Internal.PrettyPrinting where
 
-import Text.FliPpr.Internal.Type
-import Text.FliPpr.Doc
-
-import Data.Functor.Identity 
-
 import Data.Coerce
-
+import Data.Functor.Identity
+import Text.FliPpr.Doc
+import Text.FliPpr.Internal.Type
 
 data Ppr d (t :: FType) where
   PD :: d -> Ppr d D
@@ -31,22 +29,21 @@ instance DocLike d => FliPprE Identity (Ppr d) where
       go a (Branch (PartialBij _ f _) h : bs) =
         case f a of
           Nothing -> go a bs
-          Just b  -> h (Identity b) 
+          Just b -> h (Identity b)
 
-
-  funpair (Identity (a,b)) f = f (coerce a) (coerce b)
-  fununit _                x = x 
+  funpair (Identity (a, b)) f = f (coerce a) (coerce b)
+  fununit _ x = x
 
   fbchoice x _ = x
 
   ftext s = PD (text s)
-  fempty  = PD empty
+  fempty = PD empty
 
   fcat (PD d1) (PD d2) = PD (d1 <> d2)
   fline = PD line
   flinebreak = PD linebreak
 
-  fline'     = PD line
+  fline' = PD line
   fnespaces' = PD (text " ")
 
   falign (PD d) = PD (align d)
@@ -55,21 +52,36 @@ instance DocLike d => FliPprE Identity (Ppr d) where
 
   fgroup (PD d) = PD (group d)
 
-  fspace  = PD (text " ")
-  fspaces = PD empty 
+  fspace = PD (text " ")
+  fspaces = PD empty
 
-instance DocLike d => FliPprD Identity Identity (Ppr d) where
-  fshare = Identity
-  flocal = runIdentity 
-  
-  -- ffix defs = cps $ \k -> 
-  --   let x = fmap2 (\k -> runRec k x) defs
-  --   in k x 
+instance DocLike d => Defs (Ppr d) where
+  data Rules (Ppr d) _a where
+    VT :: Ppr d a -> Rules (Ppr d) (T a)
+    VP :: Rules (Ppr d) a -> Rules (Ppr d) b -> Rules (Ppr d) (a :*: b)
 
-pprModeMono :: Identity (Ppr Doc (a :~> D)) -> a -> Doc
-pprModeMono (Identity (PF h)) a =
+  lift = VT
+  unlift (VT x) = x
+
+  pairRules = VP
+  unpairRules (VP x y) k = k x y
+
+  letr h =
+    let ~(VP ~(VT a) b) = h a
+     in b
+
+-- instance DocLike d => FliPprD Identity Identity (Ppr d) where
+--   fshare = Identity
+--   flocal = runIdentity
+
+-- ffix defs = cps $ \k ->
+--   let x = fmap2 (\k -> runRec k x) defs
+--   in k x
+
+pprModeMono :: Ppr Doc (a :~> D) -> a -> Doc
+pprModeMono (PF h) a =
   case h a of
-    PD d -> d 
-                          
+    PD d -> d
+
 pprMode :: FliPpr (a :~> D) -> a -> Doc
-pprMode (FliPpr e) = pprModeMono e 
+pprMode (FliPpr e) = pprModeMono e
