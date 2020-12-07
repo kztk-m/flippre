2a43725950f32be52b2b70334c4a94c253525b33
Kazutaka Matsuda <kztk@ecei.tohoku.ac.jp>
2020-12-07 15:23:30 +0900

    Generalized to allow different representations of Γ ≦ Γ'

--- src/Text/FliPpr/Internal/Env.hs
+++ src/Text/FliPpr/Internal/Env.hs
@@ -1,40 +1,44 @@
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE PolyKinds #-}
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE PolyKinds                  #-}
+{-# LANGUAGE RankNTypes                 #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE TypeOperators              #-}
 
 -- |
 -- A file that contains manipulation of environments.
 -- The file intentionally uses some unsafe features in Haskell.
 module Text.FliPpr.Internal.Env where
 
-import Control.Applicative (Const (..))
-import Control.Category
-import Data.Functor.Identity (Identity (..))
-import Data.IntMap (IntMap)
-import qualified Data.IntMap as IM
-import Data.Kind
-import Data.Maybe (fromJust, fromMaybe)
-import Data.Monoid (Endo (..))
-import Data.Typeable (Proxy (..), (:~:) (..))
-import Text.FliPpr.Doc
-import Unsafe.Coerce
-import Prelude hiding (id, (.))
+import           Control.Applicative   (Const (..))
+import           Control.Category
+import           Data.Coerce           (coerce)
+import           Data.Functor.Identity (Identity (..))
+import           Data.IntMap           (IntMap)
+import qualified Data.IntMap           as IM
+import           Data.Kind
+import           Data.Maybe            (fromJust, fromMaybe)
+import           Data.Monoid           (Endo (..))
+import           Data.Typeable         (Proxy (..), (:~:) (..))
+import           Prelude               hiding (id, (.))
+import           Text.FliPpr.Doc
+import           Unsafe.Coerce
 
--- type Env' i t env = Env i t env env
+-- | A common implementation works in general.
+newtype VarTT i env env' = VarTT {runVarTT :: forall a. Var i env a -> Var i env' a}
 
-{- Environments that can be mutually recursive -}
-newtype VarT i env env' = VarT {runVarT :: forall a. Var i env a -> Var i env' a}
-
-class EnvImpl (i :: k -> Type) where
+class Category (VarT i) =>  EnvImpl (i :: k -> Type) where
   data Var i :: [k] -> k -> Type
   data Rep i :: [k] -> Type
   data Env i :: (k -> Type) -> [k] -> Type
+  data VarT i :: [k] -> [k] -> Type
+
+  runVarT :: VarT i env env' -> Var i env a -> Var i env' a
 
   showVar :: Var i env a -> String
   eqVar :: Var i env a -> Var i env b -> Maybe (a :~: b)
@@ -69,7 +73,7 @@ class EnvImpl (i :: k -> Type) where
 class Shiftable d t where
   shift :: VarT d env env' -> t env a -> t env' a
 
-instance Shiftable d (Var d) where
+instance EnvImpl d => Shiftable d (Var d) where
   shift = runVarT
 
 class DeBruijnIndex v where
@@ -84,9 +88,9 @@ instance DeBruijnIndex (Var U) where
 --   vz = VZ
 --   vs = VS
 
-instance Category (VarT d) where
-  id = VarT id
-  VarT f . VarT g = VarT (f . g)
+instance Category (VarTT d) where
+  id = VarTT id
+  VarTT f . VarTT g = VarTT (f . g)
 
 -- class EnvImpl i where
 --   data Var i :: [Type] -> Type -> Type
@@ -129,13 +133,22 @@ data Untype = forall a. Untype a
 unsafeCast :: Untype -> a
 unsafeCast (Untype a) = unsafeCoerce a
 
+instance Category (VarT U) where
+  id = VarTU 0
+  VarTU n . VarTU m = VarTU (n + m)
+
 instance EnvImpl U where
   newtype Var U _ _ = VarU Int
 
-  data Env U _ _ = EnvU !Int (IntMap Untype)
+  data Env U _ _ = EnvU !Int !(IntMap Untype)
 
   -- ith var corresponds to (k-i)th index.
   newtype Rep U _ = RepU Int
+
+  newtype VarT U _ _ = VarTU Int
+
+  runVarT (VarTU i) (VarU n) = VarU (i + n)
+  {-# INLINE runVarT #-}
 
   showVar (VarU n) = show n
   eqVar (VarU n) (VarU m) =
@@ -154,21 +167,71 @@ instance EnvImpl U where
   zipWithA f (EnvU k m1) (EnvU _ m2) =
     -- fmap (EnvU k) $
     --   fmap IM.fromAscList $ traverse (\((i, x), (_, y)) -> (\r -> (i, Untype r)) <$> f (unsafeCast x) (unsafeCast y)) $ zip (IM.toList m1) (IM.toList m2)
-    fmap (EnvU k) $ sequenceA $ IM.intersectionWith (\x y -> fmap Untype $ f (unsafeCast x) (unsafeCast y)) m1 m2
+    fmap (EnvU k) $ sequenceA $ IM.intersectionWith (\x y -> Untype <$> f (unsafeCast x) (unsafeCast y)) m1 m2
 
   -- EnvU k <$> sequenceA (IM.unionWith (\x y -> fmap Untype $ f <$> (unsafeCast <$> x) <*> (unsafeCast <$> y)) (fmap pure m1) (fmap pure m2))
 
   emptyEnv = EnvU (-1) IM.empty
-  extendEnv (EnvU k m) v = (EnvU (k + 1) (IM.insert (k + 1) (Untype v) m), VarU 0, VarT f)
-    where
-      f (VarU n) = VarU (n + 1)
+  {-# INLINE emptyEnv #-}
+
+  extendEnv (EnvU k m) v = (EnvU (k + 1) (IM.insert (k + 1) (Untype v) m), VarU 0, VarTU 1)
+  {-# INLINE extendEnv #-}
 
   repOf (EnvU k _) = RepU k
+  {-# INLINE repOf #-}
 
-  diffRep (RepU k) (RepU k') = VarT $ \(VarU i) -> VarU (i + (k' - k))
+  diffRep (RepU k) (RepU k') = VarTU $ k' - k
+    -- VarTU $ VarTT $ \(VarU i) -> VarU (i + (k' - k))
+  {-# INLINE diffRep #-}
 
--- data M
-data S k -- safe(r)
+
+
+-- | *U*nsafe implementing using de Bruijn levels
+data UL k
+
+instance Category (VarT UL) where
+  id    = VarTUL ()
+  _ . _ = VarTUL ()
+
+instance EnvImpl UL where
+  newtype Var UL _ _ = VarUL Int
+
+  data Env UL _ _ = EnvUL !Int !(IntMap Untype)
+
+  newtype Rep UL _ = RepUL Int
+
+  -- Here, shifting does not check any representation ...
+  newtype VarT UL _ _ = VarTUL ()
+
+  runVarT _ (VarUL x) = VarUL x
+
+  showVar (VarUL n) = show n
+  eqVar (VarUL n) (VarUL m) =
+    if n == m
+      then unsafeCoerce (Just Refl)
+      else Nothing
+
+  lookupEnv (VarUL i) (EnvUL _ m) = unsafeCast (fromJust $ IM.lookup i m)
+
+  modifyEnv (VarUL i) f (EnvUL k m) = EnvUL k (IM.adjust (Untype . f . unsafeCast) i m)
+
+  traverseWithVar f (EnvUL k m) = EnvUL k <$> IM.traverseWithKey (\i x -> Untype <$> f (coerce i) (unsafeCast x)) m
+
+  zipWithA f (EnvUL k m1) (EnvUL _ m2) =
+    fmap (EnvUL k) $ sequenceA $ IM.intersectionWith (\x y -> Untype <$> f (unsafeCast x) (unsafeCast y)) m1 m2
+
+
+  emptyEnv = EnvUL 0 IM.empty
+  extendEnv (EnvUL k m) v = (EnvUL (k + 1) (IM.insert k (Untype v) m), VarUL k, VarTUL ())
+
+  repOf (EnvUL k _) = RepUL k
+
+  diffRep _ _ = VarTUL ()
+
+
+
+-- | Safe(r) implementation
+data S k
 
 instance EnvImpl S where
   data Var S :: [k] -> k -> Type where
@@ -179,38 +242,43 @@ instance EnvImpl S where
     EEnd :: Env S f '[]
     EExtend :: Env S f env -> f a -> Env S f (a : env)
 
+  newtype VarT S env env' = VarTS (VarTT S env env')
+    deriving Category
+
   newtype Rep S env = SRep (Env S Proxy env)
 
-  eqVar VZ VZ = Just Refl
+  runVarT = runVarTT . coerce
+
+  eqVar VZ VZ         = Just Refl
   eqVar (VS n) (VS m) = eqVar n m
-  eqVar _ _ = Nothing
+  eqVar _ _           = Nothing
 
   showVar = show . go
     where
       go :: Var S env a -> Int
-      go VZ = 0
+      go VZ     = 0
       go (VS n) = 1 + go n
 
-  lookupEnv VZ (EExtend _ v) = v
+  lookupEnv VZ (EExtend _ v)     = v
   lookupEnv (VS n) (EExtend e _) = lookupEnv n e
 
-  modifyEnv VZ f (EExtend e v) = EExtend e (f v)
+  modifyEnv VZ f (EExtend e v)     = EExtend e (f v)
   modifyEnv (VS n) f (EExtend e v) = EExtend (modifyEnv n f e) v
 
   traverseWithVar _ EEnd = pure EEnd
-  traverseWithVar fn (EExtend e v) = go fn e v (VarT id)
+  traverseWithVar fn (EExtend e v) = go fn e v (coerce $ VarTT id)
     where
       go :: Applicative m => (forall t. Var S env t -> f t -> m (g t)) -> Env S f def -> f a -> VarT S (a : def) env -> m (Env S g (a : def))
       go f EEnd t sh = EExtend EEnd <$> f (runVarT sh VZ) t
       go f (EExtend rest t') t sh =
-        EExtend <$> go f rest t' (sh . VarT VS) <*> f (runVarT sh VZ) t
+        EExtend <$> go f rest t' (sh . coerce (VarTT VS)) <*> f (runVarT sh VZ) t
 
   zipWithA _ EEnd EEnd = pure EEnd
   zipWithA f (EExtend e v) (EExtend e' v') =
     EExtend <$> zipWithA f e e' <*> f v v'
 
   emptyEnv = EEnd
-  extendEnv e v = (EExtend e v, VZ, VarT VS)
+  extendEnv e v = (EExtend e v, VZ, coerce (VarTT VS))
 
   repOf = SRep . mapEnv h
     where
@@ -223,13 +291,13 @@ instance EnvImpl S where
   diffRep (SRep rep1) (SRep rep2) = go (len rep2 - len rep1) rep1 rep2
     where
       len :: Env S f env -> Int
-      len EEnd = 0
+      len EEnd          = 0
       len (EExtend e _) = len e + 1
 
       go :: Int -> Env S Proxy env -> Env S Proxy env' -> VarT S env env'
-      go 0 _ _ = unsafeCoerce $ VarT id
-      go n r1 (EExtend r2 _) = VarT VS . go (n -1) r1 r2
-      go _ _ _ = error "diffRep: violated invariant."
+      go 0 _ _               = unsafeCoerce $ VarTS $ VarTT id
+      go n r1 (EExtend r2 _) = coerce (VarTT VS) . go (n -1) r1 r2
+      go _ _ _               = error "diffRep: violated invariant."
 
 --   -- embedVar (RExtend REnd _) (RExtend REnd _) VZ     = unsafeCoerce VZ
 --   -- embedVar (RExtend REnd v) (RExtend e _)    VZ     = VS (embedVar (RExtend REnd v) e VZ)
@@ -282,15 +350,20 @@ foldEnvWithVar ::
   Env rep f def ->
   m
 foldEnvWithVar f = getConst . traverseWithVar (\v e -> Const (f v e))
+{-# INLINE foldEnvWithVar #-}
 
 foldEnv :: (EnvImpl rep, Monoid m) => (forall a. f a -> m) -> Env rep f def -> m
 foldEnv f = foldEnvWithVar (const f)
+{-# INLINE foldEnv #-}
 
 foldrEnv :: (EnvImpl rep) => (forall a. f a -> b -> b) -> b -> Env rep f def -> b
 foldrEnv f = flip $ appEndo . foldEnv (Endo . f)
+{-# INLINE foldrEnv #-}
 
 foldrEnvWithVar :: (EnvImpl rep) => (forall a. Var rep def a -> f a -> b -> b) -> b -> Env rep f def -> b
 foldrEnvWithVar f = flip $ appEndo . foldEnvWithVar (\k x -> Endo (f k x))
+{-# INLINE foldrEnvWithVar #-}
+
 
 pprEnv :: forall rep d f def. (EnvImpl rep, DocLike d) => (forall a. String -> f a -> d) -> Env rep f def -> d
 pprEnv pprBody = fromMaybe empty . foldrEnvWithVar h Nothing
@@ -300,7 +373,7 @@ pprEnv pprBody = fromMaybe empty . foldrEnvWithVar h Nothing
       let rd = align (pprBody (showVar v) body)
        in Just $ case r of
             Nothing -> rd
-            Just d -> d $$ rd
+            Just d  -> d $$ rd
 
 --   -- foldrEnvWithVar (\v body r -> r $$ prefix <> text (showVar v) <+> text "=" <+> align (pprBody prefix body))
 --   --                 empty
